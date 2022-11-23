10fd773cc722278f4f376564331b0b27e84839c3
Carl <carl0lkk@gmail.com>
2022-11-24 05:32:22 +0800

    updated home page button

--- Frontend/pages/home/index.tsx
+++ Frontend/pages/home/index.tsx
@@ -1,14 +1,23 @@
 import { 
   ChakraProvider,
-  Button
+  Button,
+  Link,
+  Box,
+  Flex
 } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
     <ChakraProvider>
+      <Box>
+        <Flex mt={4} justify="center">
+      <Link href='./createPostForm'>
     <Button colorScheme='teal' size='lg'>
     Create Post
   </Button>
+  </Link>
+  </Flex>
+  </Box>
   </ChakraProvider>
   )
   };
