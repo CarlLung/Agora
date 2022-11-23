33b90a834ec7a11a2e4316c0ba2af8a2481ec3d6
Carl <carl0lkk@gmail.com>
2022-11-23 20:03:52 +0800

    updated NavBar, createPostForm

--- Frontend/pages/home/index.tsx
+++ Frontend/pages/home/index.tsx
@@ -1,5 +1,16 @@
-const HomePage = () => {
-  return <div>This is homepage</div>;
-};
+import { 
+  ChakraProvider,
+  Button
+} from "@chakra-ui/react";
 
+const HomePage = () => {
+  return (
+    <ChakraProvider>
+    <Button colorScheme='teal' size='lg'>
+    Create Post
+  </Button>
+  </ChakraProvider>
+  )
+  };
+  
 export default HomePage;
