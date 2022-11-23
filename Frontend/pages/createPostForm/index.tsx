8e87333bc89c92a0a13016f7afc79864685669f8
Edwin Kwan <edwinkwantc@gmail.com>
2022-11-23 21:33:55 +0800

    fix: updating form type

--- Frontend/pages/createPostForm/index.tsx
+++ Frontend/pages/createPostForm/index.tsx
@@ -1,31 +1,26 @@
 import React from "react";
 import Head from "next/head";
-import { useForm } from 'react-hook-form';
-import { 
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
   ChakraProvider,
   Button,
   Select,
   Textarea,
-  Input
+  Input,
 } from "@chakra-ui/react";
-import {
-  TriangleDownIcon
-} from '@chakra-ui/icons';
+import { TriangleDownIcon } from "@chakra-ui/icons";
 import NavBar from "../../components/layout/NavBar";
 
-
-
 export default function createPostForm() {
-
-  type Post = {title: string;mainContent: string;tag: string}
+  type Post = { title: string; mainContent: string; tag: string };
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<Post>();
 
-  const onSubmit = (data: Post) => {
+  const onSubmit: SubmitHandler<Post> = (data) => {
     console.log(data);
   };
 
@@ -37,36 +32,37 @@ export default function createPostForm() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <NavBar />
-    <div className="createPostForm">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-control">
-          <label>Title</label>
-          <Input {...register("title")} />
-        </div>
-        <div className="form-control">
-          <label>Main Content</label>
-          <Textarea {...register("mainContent")} />
-        </div>
-        <div className="form-control">
-          <label>Tags</label>
-          <Select placeholder='Select option' 
-                  variant='outline'
-                  icon={<TriangleDownIcon />}
-                  {...register("tag")}>
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
-          </Select>
-        </div>
-        <div className="form-control">
-          <label></label>
-          <Button type="submit" colorScheme='teal' size='md'>Submit</Button>
-        </div>
-      </form>
-    </div>
-  </ChakraProvider>
-  )
-  };
-  
-  
-
+      <div className="createPostForm">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control">
+            <label>Title</label>
+            <Input {...register("title")} />
+          </div>
+          <div className="form-control">
+            <label>Main Content</label>
+            <Textarea {...register("mainContent")} />
+          </div>
+          <div className="form-control">
+            <label>Tags</label>
+            <Select
+              placeholder="Select option"
+              variant="outline"
+              icon={<TriangleDownIcon />}
+              {...register("tag")}
+            >
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </div>
+          <div className="form-control">
+            <label></label>
+            <Button type="submit" colorScheme="teal" size="md">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+    </ChakraProvider>
+  );
+}
