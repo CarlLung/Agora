da91ce25b0e69b2f681ead7097da803496675382
Edwin Kwan <edwinkwantc@gmail.com>
2022-12-24 19:06:05 +0800

    fix: renaming Login Form to CreatePostForm

--- Frontend/pages/login/index.tsx
+++ Frontend/pages/login/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../../styles/GlobalFont'
-import LoginForm from '../../components/auth/login'
 import Layout, { BgImg } from '../../components/layout/Layout'
 import NavBar from '../../components/layout/NavBar'
+import CreatePostForm from '../../components/Form/CreatePostForm'
 
 export default function Login() {
     return (
@@ -14,13 +14,12 @@ export default function Login() {
                 <meta name="description" content="Login" />
                 <link rel="icon" href="/images/logosq.png" />
             </Head>
-            
+
             <Layout>
                 <BgImg />
                 <NavBar />
-                <LoginForm />
+                <CreatePostForm />
             </Layout>
-            
-         </ChakraProvider>
+        </ChakraProvider>
     )
 }
