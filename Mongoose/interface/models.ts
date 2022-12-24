cb0415a0e3d4ed57c260bbed40504e4a8c0101aa
Edwin Kwan <edwinkwantc@gmail.com>
2022-12-24 17:03:38 +0800

    feat: adding jwt functions

---
+++ Mongoose/interface/models.ts
@@ -0,0 +1,22 @@
+//other
+declare global {
+    namespace Express {
+        interface Request {
+            jwtPayload?: JWTPayload
+        }
+    }
+}
+
+export type JWTPayload = {
+    user_id: number
+    username: string
+}
+
+export type NativeLoginInput = {
+    username: string
+    password: string
+}
+
+export type NativeLoginOutput = {
+    jwt_token: string
+}
