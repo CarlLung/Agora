2d5a886522b73d65f96a88b5ee68611c3570606a
Edwin Kwan <edwinkwantc@gmail.com>
2022-12-25 01:36:37 +0800

    feat: user login

---
+++ Frontend/redux/reducers/authReducer.ts
@@ -0,0 +1,70 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { store } from '../store'
+import axios from 'axios'
+
+export interface User {
+    id: string
+    username: string
+    email: string
+    token: string
+}
+
+export interface LoginInput {
+    usernameOrEmail: string
+    password: string
+}
+
+export interface AuthState {
+    user: User | null
+}
+
+const initialState: AuthState = {
+    user: null,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload
+        },
+        logout: (state) => {
+            state.user = null
+        },
+    },
+})
+
+export const localLogin = createAsyncThunk(
+    'auth/localLogin',
+    async (input: LoginInput) => {
+        const { usernameOrEmail, password } = input
+
+        try {
+            await axios
+                .post('http://localhost:8080/login', {
+                    usernameOrEmail,
+                    password,
+                })
+                .then((res) => {
+                    return store.dispatch(
+                        login({
+                            ...res.data.data,
+                        })
+                    )
+                })
+                .catch(() => {
+                    return store.dispatch(logout())
+                })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+)
+
+export const localLoginThunk = async (input: LoginInput) =>
+    await store.dispatch(localLogin(input))
+
+export const { login, logout } = authSlice.actions
+
+export default authSlice.reducer
