91e1f8b553880b02f19d6b89340eb3c632380ff7
Edwin Kwan <edwinkwantc@gmail.com>
2022-12-24 18:18:09 +0800

    feat: adding guard

--- Mongoose/routes.ts
+++ Mongoose/routes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { PostController } from './Controllers/PostsController'
 import { RegisterController } from './Controllers/RegisterController'
+import { requireLogin } from './lib/guard'
 
 export default function (args: {
     postController: PostController
@@ -11,9 +12,13 @@ export default function (args: {
 
     router.get('/posts', args.postController.getPosts)
 
-    router.post('/posts', args.postController.createPost)
+    router.post('/posts', requireLogin, args.postController.createPost)
 
-    router.get('/register', args.registerController.getUserProfile)
+    router.get(
+        '/register',
+        requireLogin,
+        args.registerController.getUserProfile
+    )
 
     router.post('/register', args.registerController.createUser)
 
